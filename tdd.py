1d41328daf66b26be16f737b1b81582f605c5146
yue wang <moonwang1999@126.com>
2020-03-31 10:29:57 +0800

    First unit test and url mapping,dumy view

--- tdd.py
+++ tdd.py
@@ -1,5 +1,38 @@
 from selenium import webdriver
-browser=webdriver.Firefox()
-browser.get('http://localhost:8000')
+import unittest
 
-assert 'Django'  in browser.title 
+class NewVisitorTest(unittest.TestCase):
+      def setUp(self):
+          self.browser = webdriver.Firefox()
+      def tearDown(self):
+          self.browser.quit()
+      def test_can_start_a_list_and_retrieve_it_later(self):
+          self.browser.get('http://localhost:8000')
+          self.assertIn('To-Do',self.browser.title)
+          self.fail('Finish the test!')
+if __name__== '__main__':
+   unittest.main(warnings='ignore')
+
+#Edith has heard about a cool new online to-do app , She goes
+#to check out its homeage
+
+
+#She notices the page title and header mention to-do lists
+
+#She is invited to enter a to-do item straight away
+#She types "Buy peacake feathers" into a text box (Edith's hobby is
+# tying fly-fishing lures)
+
+#when she hit enter, the page updates, and now the page lists
+# "1.Buy peacake feathers " is on item in a to-do lists
+
+#there is still a text box inviting her to add another item.
+#She enters "Use peacake feathers to make a fly "
+#The page updates again.and now shows both items on her lists
+
+#Edith wonders whether the site will remember her list.Then she sees
+#that the site has generated a unique URL for her -- there is some
+#explanatory text to that effect
+#She visits that URL - her to-do list is still there.
+
+#Satisfied,she goes back to sleep.
