7f67366face822af7280c8ec710f6f1cb15a650b
yue wang <moonwang1999@126.com>
2020-04-07 11:03:22 +0800

    Front page HTML now generated from a template

--- tdd.py
+++ tdd.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
       def setUp(self):
@@ -10,7 +12,7 @@ class NewVisitorTest(unittest.TestCase):
           self.browser.get('http://localhost:8000')
           self.assertIn('To-Do',self.browser.title)
           header_text = self.browser.find_element_by_tag_name('h1').text
-          self_assertIn('To-Do',header_text)
+          self.assertIn('To-Do',header_text)
           inputbox = self.browser.find_element_by_id('id_new_item')
           self.assertEqual(
               inputbox.get_attribute('placeholder'),
@@ -22,7 +24,8 @@ class NewVisitorTest(unittest.TestCase):
           table = self.browser.find_element_by_id('id_list_table')
           rows = table.find_elements_by_tag_name('tr')
           self.assertTrue(
-              any(row.text == '1:Buy peacock feathers' for row in rows)
+              any(row.text == '1:Buy peacock feathers' for row in rows),
+              "New to-do item did not appear in table"
               )
           self.fail('Finish the test!')
 if __name__== '__main__':
