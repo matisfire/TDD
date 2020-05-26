802825ab8a9ef3cb4b0d03eb04b5ab2bcc967482
zhaochangen <2464307486@qq.com>
2020-05-26 11:24:45 +0800

    use bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,30 @@
 from selenium.common.exceptions import WebDriverException
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
+      def test_layout_and_styling(self):
+       self.browser.get(self.live_server_url)
+       self.browser.set_window_size(1024, 768)
+       inputbox = self.browser.find_element_by_id('id_new_item')
+       self.assertAlmostEqual(
+           inputbox.location['x'] + inputbox.size['width'] / 2,
+           512,
+           delta=10)
+       inputbox.send_keys('testing')
+       inputbox.send_keys(Keys.ENTER)
+       self.wait_for_row_in_list_table('1: testing')
+       inputbox = self.browser.find_element_by_id('id_new_item')
+       self.assertAlmostEqual(
+           inputbox.location['x'] + inputbox.size['width'] / 2,
+           512,
+           delta=10)
+
       def setUp(self):
           self.browser = webdriver.Firefox()
       def tearDown(self):
--- lists/tests.py
+++ lists/tests.py
@@ -7,6 +7,18 @@ from lists.views import home_page
 from lists.models import Item, List
 
 class ListViewTest(TestCase):
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlomstEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
+
     def test_passes_correct_list_to_template(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
